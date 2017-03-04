009fd9fdb817b2ffcdec7f5fd29a20909ab8abe2
Karolina <szefu6@wp.pl>
2017-03-04 22:36:14 +0100

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+// plik scripts.js
+
+var a; 
+var b; 
+var value;
+
+a = prompt('liczba a');
+b = prompt('liczba b');
+value = (a*a) + (2 * a * b) - (b*b);
+
+	console.log('(a*a) + (2 * a * b) - (b*b)');
+	console.log(value); 
+document.getElementById('demo').innerHTML = value; // wyswietlam wynik poza konsola
+
+if (value > 0) { 
+	console.log('wynik dodatni'); 
+} else if (value < 0) {  
+	console.log('wynik ujemny');
+} else if (value == 0) {
+	console.log('wynik równy 0'); 
+}
+
